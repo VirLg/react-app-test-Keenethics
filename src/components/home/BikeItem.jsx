cc7662df23a0296127f3b4b69a02e6d6a2f82ccb
VirLg <19virlg@gmail.com>
2024-01-02 23:43:47 +0200

    delete

--- src/components/home/BikeItem.jsx
+++ src/components/home/BikeItem.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useDeleteBikeMutation } from '../redux/rtkQuery/bikes';
 
 const BikeItem = ({ data }) => {
+  const [deleteBike] = useDeleteBikeMutation();
   return (
     data &&
     data.map(el => {
-      return <div key={el.id}>element</div>;
+      return (
+        <div key={el.id} style={{ display: 'flex' }}>
+          <p>{el.name}</p>
+          <p>{el.type}</p>
+          <button type="button" onClick={() => deleteBike(el.id)}>
+            Del
+          </button>
+        </div>
+      );
     })
   );
 };
