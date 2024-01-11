db652f5f23fe028d94c679f83fe39df7f04c06d8
VirLg <19virlg@gmail.com>
2024-01-11 18:13:28 +0200

    claen app code

--- src/components/redux/rtkQuery/bikes.js
+++ src/components/redux/rtkQuery/bikes.js
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 // console.log('first', process.env.REACT_APP_BASE_URL);
-process.env.REACT_APP_BASE_URL;
+
 export const bikeApi = createApi({
   reducerPath: 'bikeRtk',
-  // process.env.REACT_APP_BASE_URL
+
   baseQuery: fetchBaseQuery({
     // baseUrl:https://656b819ddac3630cf7282c4a.mockapi.io/advarts/bike
     baseUrl: process.env.REACT_APP_BASE_URL,
